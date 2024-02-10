55204ea1b068337cac55cef52070d2c746ff2000
Ayush_sccg <ayush1240945@gmail.com>
2024-02-10 15:11:39 +0545

    Concise Control Flow with if let

--- src/main.rs
+++ src/main.rs
@@ -593,45 +593,51 @@
 
 // The match control flow construct
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
 
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
+// #[derive(Debug)] // so we can inspect the state in a minute
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     // --snip--
+// }
 
-fn main() {
-    let dice_roll = 9;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
-        other => move_player(other),
-    }
-}
+// fn main() {
+//     let dice_roll = 9;
+//     match dice_roll {
+//         3 => add_fancy_hat(),
+//         7 => remove_fancy_hat(),
+//         other => move_player(other),
+//     }
+// }
 
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
-fn move_player(num_spaces: u8) {}
+// fn add_fancy_hat() {}
+// fn remove_fancy_hat() {}
+// fn move_player(num_spaces: u8) {}
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
-}
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
+
+// if let control flow
+fn main(){
+    
 }
