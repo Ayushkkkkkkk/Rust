e34660a5d16137f036d8937308020a25a1aa5a5e
Ayush_sccg <ayush1240945@gmail.com>
2024-02-10 15:16:50 +0545

    __

--- src/main.rs
+++ src/main.rs
@@ -638,16 +638,16 @@
 
 // Concise Control Flow with if let
 
-fn main() {
-    let config_max = Some(3u8);
-    if let Some(max) = config_max {
-        println!("The maximum is configured to be {}", max);
-    }
+// fn main() {
+//     let config_max = Some(3u8);
+//     if let Some(max) = config_max {
+//         println!("The maximum is configured to be {}", max);
+//     }
 
-    let mut count = 0;
-    if let Coin::Quarter(state) = coin {
-        println!("State quarter from {:?}!", state);
-    } else {
-        count += 1;
-    }
-}
+//     let mut count = 0;
+//     if let Coin::Quarter(state) = coin {
+//         println!("State quarter from {:?}!", state);
+//     } else {
+//         count += 1;
+//     }
+// }
