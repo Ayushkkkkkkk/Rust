1d289c721bc6502285c8509dcc046267b7b38092
Ayush_sccg <ayush1240945@gmail.com>
2024-02-10 15:11:08 +0545

    match Control flow

--- src/main.rs
+++ src/main.rs
@@ -540,7 +540,6 @@
 //     // --snip--
 // }
 
-
 // enum IpAddr3 {
 //     V4(Ipv4Addr),
 //     V6(Ipv6Addr),
@@ -563,8 +562,6 @@
 //     None,
 //     Some(T),
 // }
-
-
 
 // fn main() {
 //     let four = IpAddrKind::V4;
@@ -594,5 +591,47 @@
 
 // fn route(ip_kind: IpAddrKind) {}
 
-
 // The match control flow construct
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
