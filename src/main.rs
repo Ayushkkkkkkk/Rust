2ae7cbbd2cca156d24e1ef0614b7157a952eeb16
Ayush_sccg <ayush1240945@gmail.com>
2024-02-10 14:51:26 +0545

    enum

--- src/main.rs
+++ src/main.rs
@@ -473,46 +473,123 @@
 // }
 
 // Method syntax
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+//     fn square(size: u32) -> Self {
+//         Self {
+//             width: size,
+//             height: size,
+//         }
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         rect1.area()
+//     );
+//     let rect2 = Rectangle {
+//         width: 10,
+//         height: 40,
+//     };
+//     let rect3 = Rectangle {
+//         width: 60,
+//         height: 45,
+//     };
+
+//     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+//     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+// }
+
+// enums and pattern matching
+enum IpAddrKind {
+    V4,
+    V6,
+}
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+struct Ipv4Addr {
+    // --snip--
+}
+
+struct Ipv6Addr {
+    // --snip--
+}
+
+
+enum IpAddr3 {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
     }
 }
+
+enum Option<T> {
+    None,
+    Some(T),
+}
+
+
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
     };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr2::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    // let absent_number: Option<i32> = None;
 }
+
+fn route(ip_kind: IpAddrKind) {}
