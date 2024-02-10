d8c41a9e398adef5107ed1990dcd0f462f96e6df
Ayush_sccg <ayush1240945@gmail.com>
2024-02-10 15:16:26 +0545

    Concise Control Flow with if let

--- src/main.rs
+++ src/main.rs
@@ -636,8 +636,18 @@
 //     }
 // }
 
+// Concise Control Flow with if let
 
-// if let control flow
-fn main(){
-    
+fn main() {
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
 }
