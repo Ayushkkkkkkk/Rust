8639a8965d4118b5024cb4b861dd120d5ceb9540
Ayush_sccg <ayush1240945@gmail.com>
2024-02-18 14:12:10 +0545

    map

--- src/main.rs
+++ src/main.rs
@@ -675,17 +675,17 @@
 //     let s = "initial contents".to_string();
 // }
 
-use std::collections::HashMap;
-fn main() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("blue"), 0);
-    scores.insert(String::from("yellow"), 0);
+//     let mut scores = HashMap::new();
+//     scores.insert(String::from("blue"), 0);
+//     scores.insert(String::from("yellow"), 0);
+//
+//     let team_name = String::from("blue");
+//     let score = scores.get(&team_name).copied().unwrap(0);
+//     for (key, value) in &scores {
+//         println!("{key}: {value}");
+//     }
+//     scores.entry(String::from("Yellow")).or_insert(50);
+//     scores.entry(String::from("Blue")).or_insert(50);
+// }
 
-    let team_name = String::from("blue");
-    let score = scores.get(&team_name).copied().unwrap(0);
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-}
+fn main() {}
