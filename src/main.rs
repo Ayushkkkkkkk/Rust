a0ed7c46200e32d5b01d85256366240d1b0bd541
Ayush_sccg <ayush1240945@gmail.com>
2024-02-19 13:47:19 +0545

    Error handeling

--- src/main.rs
+++ src/main.rs
@@ -688,4 +688,39 @@
 //     scores.entry(String::from("Blue")).or_insert(50);
 // }
 
-fn main() {}
+// error handeling
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problems opening the file: {:?}", error),
+    };
+    // //     // let greeting_file_2 = match error.kind(){
+    // //     //     ErrorKind::NotFound => match File::create("hello.txt") {
+    // //     //         Ok(fx) => fx,
+    // //     //         Err(e) => panic!("Proble creating the file {}" , e);
+    // //     //     },
+    // //     //     other_error => {
+    // //     //         panic!("Proble opeing the file {}" , other_error);
+    // //     //     }
+    // };
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
