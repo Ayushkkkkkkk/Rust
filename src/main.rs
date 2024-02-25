29392ba1154a86224b56a3518b058972bfb2b0a1
Ayush_sccg <ayush1240945@gmail.com>
2024-02-25 16:07:11 +0545

    generic types

--- src/main.rs
+++ src/main.rs
@@ -723,18 +723,135 @@
 //     }
 // }
 //
+// generics
+// fn main() {
 //
-fn main() {}
+// }
+//
+// fn duplications() {
+//     let number_list = vec![43, 34, 43, 32333, 32334234];
+//
+//     let mut largest = &number_list[0];
+//
+//     for number in &number_list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+//     println!("The largest number is {}", largest);
+//
+//
+//     let number_list = vec![3434,4,34,3,43,4,34,34,3,43,4,234,234,234,90];
+//
+//     let result = largest(&number_list);
+//     println("The largest number is {}" , result);
+// }
+//
+// fn largest(list:&[i32]) -> &i32 {
+//     let mut largest = &list[0];
+//
+//     for item in list{
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//
+//     largest
+//
+// }
+//
 
-fn duplications() {
-    let number_list = vec![43, 34, 43, 32333, 32334234];
+// generic data type
 
-    let mut largest = &number_list[0];
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
-    println!("The largest number is {}", largest);
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 }
